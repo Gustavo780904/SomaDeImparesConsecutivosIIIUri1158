ad665c56d990e7ec2d909357443aa26ba37ce261
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-14 01:19:52 -0300

    Projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	Locale.setDefault(Locale.US);
-	Scanner sc =new Scanner(System.in);
-	
-	
-	
-	sc.close();
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int N, X = 0, Y = 0, soma = 0;
+		N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			X = sc.nextInt();
+			Y = sc.nextInt();
+			
+				if (X % 2 == 1) {
+					soma = X * Y + Y * (Y - 1); 
+				}else {
+					soma = X * Y + Y * Y;
+				}
+			
+			System.out.println(soma);
+		}
+
+		sc.close();
 
 	}
 
